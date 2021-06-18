1f48cc57bed02cc97c7f37156368362cf328d431
pradeeshkarun <85067954+pradeeshkarun@users.noreply.github.com>
2021-06-18 16:36:39 +0530

    Solu_12

--- solns.c
+++ solns.c
@@ -1,55 +1,5 @@
 
 #include <stdio.h>
-#include <assert.h>
-
-
-int max(int [], int);
-int min(int [], int);
-float average(int [], int);
-int mode(int [], int);
-
-
-
-int main(void) {
-  
-  int x1[] = {9,5,6,10,2,-3,4};
-  assert (max(x1, 7) == 10);
-
-  int y1[] = {5};
-  assert (max(y1, 1) == 5);
-  printf("Max: passed\n");
-  
-  //-------------------------
-
-  int x2[] = {9,5,6,10,2,-3,4};
-  assert (min(x2, 7) == -3);
-
-  int y2[] = {5};
-  assert (min(y2, 1) == 5);
-  printf("Min: passed\n");
-  
-  //------------------------
-  
-
-  int x3[] = {9,5,6,10,2,-3,4};
-  assert ((average(x3, 7) - 4.7142) < 0.001);
-
-  int y3[] = {5};
-  assert (average(y3, 1) == 5.0);
-  printf("Average: passed\n");
-  
-  
-  //---------------------------
-  
-  
-  int x4[] = {4, 9,5,6,5,10,0,2,-3, -3,4, 4};
-  assert ((mode(x4, 12) == 4));
-
-  int y4[] = {5};
-  assert (mode(y4, 1) == 5);
-  printf("Mode: passed\n");
-  
-}
   
   //FUNCTIONS////////////////////////////////////////////////////////////////////////
 
@@ -90,7 +40,8 @@ int min(int x[], int n){
 //AVG Function-------------------------------------------------------
 
 
-float average(int x[], int n){
+float average(int x[], int n)
+{
   
   float element_sum=0;
   
